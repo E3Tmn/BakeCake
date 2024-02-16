01bf40468e680030ba334bc3edfc22f64ead3606
duke-doki <145125297+duke-doki@users.noreply.github.com>
2024-02-16 11:56:39 +0300

    add favicon

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def lk_order(request):
     return render(request, 'lk-order.html')
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def lk(request):
+    return render(request, 'lk.html')
+
