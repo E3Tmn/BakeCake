d3eb050c36a664aff7edf2e7e96737d68620e059
duke-doki <145125297+duke-doki@users.noreply.github.com>
2024-02-17 15:58:03 +0300

    make index.html great again

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -8,45 +8,7 @@ def lk_order(request):
 
 
 def index(request):
-    DATA = {
-        'Levels': {
-            'num1': '1',
-            'num2': '2',
-            'num3': '3'
-        },
-        'Forms': {
-            'form1': 'Круг',
-            'form2': 'Квадрат',
-            'form3': 'Прямоугольник'
-        },
-        'Toppings': {
-            'topping1': 'Без',
-            'topping2': 'Белый соус',
-            'topping3': 'Карамельный',
-            'topping4': 'Кленовый',
-            'topping5': 'Черничный',
-            'topping6': 'Молочный шоколад',
-            'topping7': 'Клубничный'
-        },
-        'Berries': {
-            'berries1': 'Ежевика',
-            'berries2': 'Малина',
-            'berries3': 'Голубика',
-            'berries4': 'Клубника'
-        },
-        'Decors': {
-            'decor1': 'Фисташки',
-            'decor2': 'Безе',
-            'decor3': 'Фундук',
-            'decor4': 'Пекан',
-            'decor5': 'Маршмеллоу',
-            'decor6': 'Марципан',
-
-
-        },
-        'Words': 'С днем рождения!'
-    }
-    return render(request, 'index.html', {'DATA': DATA})
+    return render(request, 'index.html')
 
 
 def lk(request):
